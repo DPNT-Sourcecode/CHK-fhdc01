21abd806c591ed3bc8f3436543658f89b5454f48
sbx_user1051 <sbx_user1051@169.254.3.113>
2025-10-24 22:38:29 +0000

    Fri Oct 24 22:38:29 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,9 +3,10 @@ class SumSolution:
 
     def compute(self, number_one: int, number_two: int) -> int:
         if not isinstance(number_one, int) or not isinstance(number_two, int) \
-            or not (0 < number_one < 100) or not (0 < number_two < 100):
+            or not (0 <= number_one <= 100) or not (0 <= number_two <= 100):
             raise ValueError("Both inputs must be integers between 0 and 100.")
         return number_one + number_two
+
 
 
 
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -7,11 +7,12 @@ class TestSum():
         assert SumSolution().compute(1, 2) == 3
         assert SumSolution().compute(10, 30) == 40
         assert SumSolution().compute(1, 99) == 100
+        assert SumSolution().compute(0, 0) == 0
         assert pytest.raises(ValueError, SumSolution().compute, -1, 1)
-        assert pytest.raises(ValueError, SumSolution().compute, 0, 0)
         assert pytest.raises(ValueError, SumSolution().compute, 0, 101)
         assert pytest.raises(ValueError, SumSolution().compute, 5, -6)
         assert pytest.raises(ValueError, SumSolution().compute, 5, -4)
+
 
 
 
