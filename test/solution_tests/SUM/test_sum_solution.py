fa28e570e6d6d888f326f224cb3f3d843aaf7983
sbx_user1051 <sbx_user1051@169.254.3.113>
2025-10-24 22:37:34 +0000

    Fri Oct 24 22:37:34 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -5,11 +5,14 @@ from solutions.SUM.sum_solution import SumSolution
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
-        assert SumSolution().compute(-1, 1) == 0
+        assert SumSolution().compute(10, 30) == 40
+        assert SumSolution().compute(1, 99) == 100
+        assert pytest.raises(ValueError, SumSolution().compute, -1, 1)
         assert pytest.raises(ValueError, SumSolution().compute, 0, 0)
         assert pytest.raises(ValueError, SumSolution().compute, 0, 101)
         assert pytest.raises(ValueError, SumSolution().compute, 5, -6)
         assert pytest.raises(ValueError, SumSolution().compute, 5, -4)
+
 
 
 
