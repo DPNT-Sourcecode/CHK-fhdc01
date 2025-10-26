c096786eff289133e5c0e6cce53b62badcd3c4c1
sbx_user1051 <sbx_user1051@169.254.54.41>
2025-10-26 21:29:49 +0000

    Sun Oct 26 21:29:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,10 @@
+import json
 
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
+        prices = None
+        with open("lib/solutions/CHK/prices.json") as f:
+            prices = json.load(f)
         raise NotImplementedError()
