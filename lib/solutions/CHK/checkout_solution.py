6a6458d4b3964f7c39f0f9f0240d6ff2e2b43a4a
sbx_user1051 <sbx_user1051@169.254.54.41>
2025-10-26 23:46:31 +0000

    Sun Oct 26 23:46:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,11 +13,6 @@ class CheckoutSolution:
         self.basket_items: list[BasketItem] = []
         self.basket_items_offer_applied: list[AnalysedBasketItem] = []
 
-    def add_to_total(self, amount: int):
-        if amount is None:
-            self.error = True
-        self.total += amount
-
     def get_prices(self) -> list[RawPrice]:
         with open("lib/solutions/CHK/prices.json") as f:
             prices: list[RawPrice] = json.load(f)
@@ -39,12 +34,19 @@ class CheckoutSolution:
             }
             self.basket_items.append(item)
 
+    def get_item_price(self, sku: str) -> int | None:
+        sku_price = next((price for price in self.prices if price["sku"] == sku), None)
+        if not sku_price:
+            self.error = True
+            return
+        return sku_price["price"]
+
     def apply_bulk_buy_offer(self, offer: Offer) -> NoReturn:
         for item in self.basket_items:
             if item["sku"] == offer["sku"] and item["quantity"] >= offer["quantity"]:
                 num_offers = item["quantity"] // offer["quantity"]
                 remainder = item["quantity"] % offer["quantity"]
-                adjusted_price = (num_offers * offer["price"]) + (remainder * self.calculate_item_price(self.prices, item))
+                adjusted_price = (num_offers * offer["price"]) + (remainder * self.get_item_price(item["sku"]))
                 analysed_item: AnalysedBasketItem = {
                     "sku": item["sku"],
                     "quantity": item["quantity"],
@@ -56,19 +58,8 @@ class CheckoutSolution:
         for offer in self.offers:
             if offer["offer_type"] == "bulk_buy":
                 self.apply_bulk_buy_offer(offer)
-        
-    def calculate_item_price(self, prices: list[RawPrice], item) -> int | None:
-        sku_price = next((price for price in prices if price["sku"] == item["sku"]), None)
-        # Catch invalid SKU
-        if not sku_price:
-            return None
-        
-        # Check for special offer pricing including multiples
-        if sku_price["special_offer"]:
-            num_offers = item["quantity"] // sku_price["special_offer"]["quantity"]
-            remainder = item["quantity"] % sku_price["special_offer"]["quantity"]
-            return (num_offers * sku_price["special_offer"]["price"]) + (remainder * sku_price["price"])
-        return sku_price["price"] * item["quantity"]
+            if offer["offer_type"] == "free_item":
+                pass
 
     # skus = unicode string
     def checkout(self, skus: str) -> int:
@@ -79,4 +70,5 @@ class CheckoutSolution:
             # the adjusted price should be added to the item in the basket
             # Calculate total price using adjusted prices
         return -1
+
 
