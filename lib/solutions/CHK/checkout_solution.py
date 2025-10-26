a3c1cb363fc194a5caa8f13950a23ec4c2e4ad92
sbx_user1051 <sbx_user1051@169.254.54.41>
2025-10-26 21:32:49 +0000

    Sun Oct 26 21:32:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,4 +7,10 @@ class CheckoutSolution:
         prices = None
         with open("lib/solutions/CHK/prices.json") as f:
             prices = json.load(f)
+        sku_with_quantity = [{"sku": sku, "quantity": skus.count(sku)} for sku in set(skus)]
+        for sku in sku_with_quantity:
+            if sku["sku"] not in prices:
+                return -1
+            if prices[sku][]
         raise NotImplementedError()
+
