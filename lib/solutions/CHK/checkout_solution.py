71c823d35c8f472f463d9b0e0c20be5205f16e0b
sbx_user1051 <sbx_user1051@169.254.54.41>
2025-10-26 22:58:31 +0000

    Sun Oct 26 22:58:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,28 +1,29 @@
 import json
 
-from .types import Price, Offers
+from .types import RawPrice, Offers, AnalysedBasketItem
 
 class CheckoutSolution:
     def __init__(self):
         self.skus: list[str] = []
-        self.prices: list[Price] = self.get_prices()
+        self.prices: list[RawPrice] = self.get_prices()
         self.offers: Offers = self.get_offers()
         self.total: int = 0
         self.error: bool = False
+        self.basket_items: list[AnalysedBasketItem] = []
 
     def add_to_total(self, amount: int):
         if amount is None:
             self.error = True
         self.total += amount
 
-    def get_prices(self) -> list[Price]:
+    def get_prices(self) -> list[RawPrice]:
         with open("lib/solutions/CHK/prices.json") as f:
-            prices: list[Price] = json.load(f)
+            prices: list[RawPrice] = json.load(f)
         return prices
     
-    def get_offers(self) -> list[Price]:
+    def get_offers(self) -> list[RawPrice]:
         with open("lib/solutions/CHK/offers.json") as f:
-            offers: list[Price] = json.load(f)
+            offers: list[RawPrice] = json.load(f)
         return offers
 
     def get_item_quantity(self, skus: str) -> dict[str, int]:
@@ -31,7 +32,7 @@ class CheckoutSolution:
     def calculate_offer_price(self):
         pass
     
-    def calculate_item_price(self, prices: list[Price], item) -> int | None:
+    def calculate_item_price(self, prices: list[RawPrice], item) -> int | None:
         sku_price = next((price for price in prices if price["sku"] == item["sku"]), None)
         # Catch invalid SKU
         if not sku_price:
@@ -48,12 +49,14 @@ class CheckoutSolution:
     def checkout(self, skus: str) -> int:
         self.skus = list(skus)
         while not self.error:
-            item_with_quantity = self.get_item_quantity(skus)
-            for item in item_with_quantity:
-                self.add_to_total(self.calculate_item_price(prices, item))
-            if None in totals:
-                return -1
-            return sum(totals)
+            self.analyse_basket()
+            # item_with_quantity = self.get_item_quantity(skus)
+            # for item in item_with_quantity:
+            #     self.add_to_total(self.calculate_item_price(prices, item))
+            # if None in totals:
+            #     return -1
+            # return sum(totals)
         return -1
+
 
 
--- lib/solutions/CHK/types.py
+++ lib/solutions/CHK/types.py
@@ -12,12 +12,19 @@ class FreeItemOffer(TypedDict):
     free_sku: str
     free_quantity: int
 
-class Price(TypedDict):
+class RawPrice(TypedDict):
     sku: str
     price: int
 
 class Offers(TypedDict):
     bulk_buys: list[BulkOffer]
     free_item_offers: list[FreeItemOffer]
+
+class AnalysedBasketItem(TypedDict):
+    sku: str
+    quantity: int
+    price: int
+    adjusted_price: int
+
 
 
