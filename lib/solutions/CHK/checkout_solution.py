f61d7d763b0578dffcd6f0ef99694347ec15c6da
sbx_user1051 <sbx_user1051@169.254.54.41>
2025-10-26 21:38:49 +0000

    Sun Oct 26 21:38:49 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,23 +2,30 @@ import json
 
 from typing import TypedDict
 
-class Sku(TypedDict):
+class Price(TypedDict):
     sku: str
     quantity: int
     special_offer: dict | None
 
 class CheckoutSolution:
 
+    def get_prices(self) -> list[Price]:
+        with open("lib/solutions/CHK/prices.json") as f:
+            prices: list[Price] = json.load(f)
+        return prices
+
+    def get_sku_quantity(self, skus: str) -> dict[str, int]:
+        return [{"sku": sku, "quantity": skus.count(sku)} for sku in set(skus)]
+
     # skus = unicode string
     def checkout(self, skus):
-        prices = None
-        with open("lib/solutions/CHK/prices.json") as f:
-            prices = json.load(f)
-        sku_with_quantity = [{"sku": sku, "quantity": skus.count(sku)} for sku in set(skus)]
+        prices = self.get_prices()
+        sku_with_quantity = self.get_sku_quantity(skus)
         for sku in sku_with_quantity:
             if sku["sku"] not in prices:
                 return -1
-            if prices[sku][]
+            if prices:
         raise NotImplementedError()
+
 
 
